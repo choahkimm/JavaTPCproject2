f742699c3880d8b566fac21dd7c5e9dd419f5d8f
choahkimm <choahk.dev@gmail.com>
2022-01-13 23:40:18 +0900

    22/01/13

---
+++ src/Project03_D.java
@@ -0,0 +1,9 @@
+public class Project03_D {
+    public static void main(String[] args) {
+        // Naver Book Open API 사용하기
+        // 엑셀 데이터--HttpURLConnection---Naver Search API 연동
+
+
+        // 도서 정보를 입력하여 ISBN, Image 검출하기
+    }
+}
